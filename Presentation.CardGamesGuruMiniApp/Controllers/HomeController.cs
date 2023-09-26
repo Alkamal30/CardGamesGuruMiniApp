ced5deb2056b2341932e69d3f8bbf1b35559e269
Alkamal30 <yurevich.vlad3005@gmail.com>
2023-09-26 22:21:33 +0300

    Remove unnecessary code

--- Presentation.CardGamesGuruMiniApp/Controllers/HomeController.cs
+++ Presentation.CardGamesGuruMiniApp/Controllers/HomeController.cs
@@ -2,14 +2,7 @@
 
 namespace WebApp.Controllers;
 public class HomeController : Controller {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger) {
-        _logger = logger;
-    }
-
     public IActionResult Index() {
         return View();
     }
-
 }
