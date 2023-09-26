231e483e11506e2b1e6ba883329f02e2732d8a24
Alkamal30 <yurevich.vlad3005@gmail.com>
2023-09-26 22:15:45 +0300

    Prepare frontend for Telegram MiniApp

---
+++ WebApp/wwwroot/js/index.js
@@ -0,0 +1,16 @@
+﻿const tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.text = 'Reload Page';
+tg.MainButton.show();
+
+Telegram.WebApp.onEvent('mainButtonClicked', () => {
+    tg.MainButton.showProgress();
+    document.getElementsByTagName('body')[0].style.background = "linear-gradient(217deg, rgba(255,0,0,1), rgba(255,0,0,0) 70.71%), linear-gradient(127deg, rgba(0,255,0,1), rgba(0,255,0,0) 70.71%), linear-gradient(336deg, rgba(0,0,255,1), rgba(0,0,255,0) 70.71%)";
+
+    setTimeout(() => {
+        tg.MainButton.hideProgress();
+        location.reload();
+    }, 1500);
+});
