ba73fa029be55139f950a2407f111bb1f72319d6
Alkamal30 <yurevich.vlad3005@gmail.com>
2023-09-28 00:58:02 +0300

    Add example reload page logic for MainButton

--- Presentation.CardGamesGuruMiniApp/wwwroot/js/index.js
+++ Presentation.CardGamesGuruMiniApp/wwwroot/js/index.js
@@ -1,5 +1,16 @@
-﻿let tg = window.Telegram.WebApp;
+﻿const tg = window.Telegram.WebApp;
 
-tg.MainButton.text = "Main Button";
+tg.MainButton.text = "Reload Page";
 tg.MainButton.color = "#31B545";
 tg.MainButton.show();
+tg.MainButton.showProgress();
+
+Telegram.WebApp.onEvent('mainButtonClicked', function () {
+    location.reload();
+});
+
+window.onload = (event) => {
+    setTimeout(() => {
+        tg.MainButton.hideProgress();
+    }, 500);
+};
