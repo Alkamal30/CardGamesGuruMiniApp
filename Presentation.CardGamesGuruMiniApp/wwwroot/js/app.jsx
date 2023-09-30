fd3158efb0e91175394efcc27dc22f159d9788c0
Alkamal30 <yurevich.vlad3005@gmail.com>
2023-09-30 23:43:02 +0300

    Add ThisOrThat card

--- Presentation.CardGamesGuruMiniApp/wwwroot/js/app.jsx
+++ Presentation.CardGamesGuruMiniApp/wwwroot/js/app.jsx
@@ -1,9 +1,38 @@
-class Hello extends React.Component {
-    render() {
-        return <h1>Hello, React.JS</h1>;
-    }
+function Card() {
+    return (
+        <div className="card">
+            <div className="card-head">
+                This or That
+            </div>
+            <div className="card-body">
+                <div className="card-body__question">
+                    Go back in time and meet your ancestors
+                </div>  
+                <div className="card-body__split-line">
+                    <div className="card-body__split-circle">
+                        <p>or</p>
+                    </div>
+                </div>
+                <div className="card-body__question">
+                    Go to the future and meet your great grandchildren  
+                </div>  
+            </div>
+        </div>
+    );
 }
+
+function App() {
+
+    
+
+    return (
+        <div className="card-block">
+            <Card />
+        </div>
+    );
+}
+
 ReactDOM.render(
-    <Hello />,
-    document.getElementById("content")
+    <App />,
+    document.getElementById("root")
 );
