57fcef5120de3ee3707892e7b74752953521372c
Alkamal30 <yurevich.vlad3005@gmail.com>
2023-10-01 21:24:27 +0300

    Move Telegram.WebApp importing to app.jsx. Change background color. Make simple logic of cards changing

--- Presentation.CardGamesGuruMiniApp/wwwroot/js/app.jsx
+++ Presentation.CardGamesGuruMiniApp/wwwroot/js/app.jsx
@@ -1,4 +1,45 @@
+const tg = window.Telegram.WebApp;
+
+tg.expand();
+tg.enableClosingConfirmation();
+
+tg.MainButton.text = "Next Card";
+tg.MainButton.color = "#31B545";
+tg.MainButton.show();
+tg.BackButton.show();
+
+
+let questions = ['First', 'Second', 'Third'];
+
+function nxtCard() {
+
+}
+
+
 function Card() {
+    const [questionIndex, setQuestionIndex] = React.useState(0);
+    const [firstQuestion, setFirstQuestion] = React.useState(questions[0]);
+    const [secondQuestion, setSecondQuestion] = React.useState(questions[0]);
+
+    React.useEffect(() => {
+        tg.MainButton.onClick(nextCard);
+        return () => {
+            tg.MainButton.offClick(nextCard);
+        }
+    }, [nextCard])
+
+    function nextCard() {
+        if(questionIndex + 1 >= questions.length) {
+            setQuestionIndex(0);
+        }
+        else {
+            setQuestionIndex(questionIndex + 1);
+        }
+
+        setFirstQuestion(questions[questionIndex]);
+        setSecondQuestion(questions[questionIndex]);
+    }
+    
     return (
         <div className="card">
             <div className="card-head">
@@ -6,7 +47,7 @@ function Card() {
             </div>
             <div className="card-body">
                 <div className="card-body__question">
-                    Go back in time and meet your ancestors
+                    { firstQuestion }
                 </div>  
                 <div className="card-body__split-line">
                     <div className="card-body__split-circle">
@@ -14,17 +55,14 @@ function Card() {
                     </div>
                 </div>
                 <div className="card-body__question">
-                    Go to the future and meet your great grandchildren  
-                </div>  
+                    { secondQuestion }
+                </div>
             </div>
         </div>
     );
 }
 
 function App() {
-
-    
-
     return (
         <div className="card-block">
             <Card />
--- Presentation.CardGamesGuruMiniApp/wwwroot/js/index.js
+++ Presentation.CardGamesGuruMiniApp/wwwroot/js/index.js
@@ -1,16 +1,1 @@
-﻿const tg = window.Telegram.WebApp;
-
-tg.MainButton.text = "Reload Page";
-tg.MainButton.color = "#31B545";
-tg.MainButton.show();
-tg.MainButton.showProgress();
-
-Telegram.WebApp.onEvent('mainButtonClicked', function () {
-    location.reload();
-});
-
-window.onload = (event) => {
-    setTimeout(() => {
-        tg.MainButton.hideProgress();
-    }, 500);
-};
+﻿
