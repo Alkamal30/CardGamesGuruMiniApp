24aff760b4d3e6ef5429b33085ebc8848163b796
Alkamal30 <yurevich.vlad3005@gmail.com>
2023-10-03 00:15:46 +0300

    Add menu with all card games

--- Presentation.CardGamesGuruMiniApp/wwwroot/js/app.jsx
+++ Presentation.CardGamesGuruMiniApp/wwwroot/js/app.jsx
@@ -3,58 +3,89 @@ const tg = window.Telegram.WebApp;
 tg.expand();
 tg.enableClosingConfirmation();
 
-tg.MainButton.text = "Next Card";
 tg.MainButton.color = "#31B545";
-tg.MainButton.show();
+tg.MainButton.hide();
 
 
-function Card() {
-    const [firstQuestion, setFirstQuestion] = React.useState('Example First Question');
-    const [secondQuestion, setSecondQuestion] = React.useState('Example Second Question');
+// function Card() {
+//     const [firstQuestion, setFirstQuestion] = React.useState('Example First Question');
+//     const [secondQuestion, setSecondQuestion] = React.useState('Example Second Question');
+
+//     React.useEffect(() => {
+//         tg.MainButton.onClick(nextCard);
+//         return () => {
+//             tg.MainButton.offClick(nextCard);
+//         }
+//     }, [nextCard])
+
+//     function nextCard() {
+//         fetch('/api/tot/cardrandom/')
+//             .then(response => response.json())
+//             .then(card => {
+//                 console.log(card);
+//                 setFirstQuestion(card.firstQuestion);
+//                 setSecondQuestion(card.secondQuestion);
+//             });
+//     }
+    
+//     return (
+//         <div className="card">
+//             <div className="card-head">
+//                 This or That
+//             </div>
+//             <div className="card-body">
+//                 <div className="card-body__question">
+//                     { firstQuestion }
+//                 </div>  
+//                 <div className="card-body__split-line">
+//                     <div className="card-body__split-circle">
+//                         <p>or</p>
+//                     </div>
+//                 </div>
+//                 <div className="card-body__question">
+//                     { secondQuestion }
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
+
+function Game(props) {
+    return (
+       <div className="game">
+            <div className="game-title">{props.title}</div>
+       </div> 
+    );
+}
+
+function GamesList() {
+    const [allGames, setAllGames] = React.useState([]);
 
     React.useEffect(() => {
-        tg.MainButton.onClick(nextCard);
-        return () => {
-            tg.MainButton.offClick(nextCard);
-        }
-    }, [nextCard])
+        getAllGames();
+    });
 
-    function nextCard() {
-        fetch('/api/tot/cardrandom/')
+    function getAllGames() {
+        fetch('/api/game/games')
             .then(response => response.json())
-            .then(card => {
-                console.log(card);
-                setFirstQuestion(card.firstQuestion);
-                setSecondQuestion(card.secondQuestion);
+            .then(games => {
+                setAllGames(games);
             });
     }
-    
+
     return (
-        <div className="card">
-            <div className="card-head">
-                This or That
-            </div>
-            <div className="card-body">
-                <div className="card-body__question">
-                    { firstQuestion }
-                </div>  
-                <div className="card-body__split-line">
-                    <div className="card-body__split-circle">
-                        <p>or</p>
-                    </div>
-                </div>
-                <div className="card-body__question">
-                    { secondQuestion }
-                </div>
-            </div>
+        <div className="games-list">
+            {
+                allGames.map(game => <Game title={game.name} />)
+            }
         </div>
     );
 }
 
 function App() {
     return (
-        <div className="card-block">
-            <Card />
+        <div className="app">
+            <GamesList />
         </div>
     );
 }
